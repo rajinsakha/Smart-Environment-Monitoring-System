36957824399d2a1e851d5e4668364ee4139bde72
Rajin Sakha <rajinsakha07@gmail.com>
2024-03-29 22:37:54 +0545

    fixed-padding

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between background">
       <Header />
-      <section className="py-16 px-24 grid lg:grid-cols-4 md:grid-cols-3 min-[500px]:grid-cols-2 grid-cols-1 gap-8">
+      <section className="padding grid lg:grid-cols-4 md:grid-cols-3 min-[500px]:grid-cols-2 grid-cols-1 gap-8">
       <SensorCard title="Flame" Icon={FaFireFlameCurved} value="Fire Detected" />
       <SensorCard title="Flame" Icon={FaFireFlameCurved} value="Fire Detected" />
       <SensorCard title="Flame" Icon={FaFireFlameCurved} value="Fire Detected" />
